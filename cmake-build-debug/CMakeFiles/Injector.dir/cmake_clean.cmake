46e44fc60c94d1ec10ae097f6911115a93332150
DavidRomanovizc <dtdzhalaev@gmail.com>
2021-12-07 23:30:56 +0300

    Cmake

---
+++ cmake-build-debug/CMakeFiles/Injector.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Injector.dir/src/CheatInjector.cpp.obj"
+  "Injector.exe"
+  "Injector.exe.manifest"
+  "Injector.pdb"
+  "libInjector.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Injector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
